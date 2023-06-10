6ad4a4c9a3d511bc956ab5ef13022b6b6dbe1857
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-10 11:29:50 +0530

    if-else if ladder

---
+++ student2.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+public class student2 {
+    //if-else if ladder
+    public static void main(String[]args){
+        String city="Delhi";
+        if(city=="Meerut"){
+            System.out.println("City is Meerut");
+        }else if(city=="Noida"){
+            System.out.println("City is Noida");
+        }else if(city=="Agra"){
+            System.out.println("City is Agra");
+        }else{
+            System.out.println(city);
+        }
+    }
+}
