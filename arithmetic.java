030ffddea054b902935b7bb74a7ac92706f3004e
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-11 16:25:32 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+package JAVA;
+
+public class arithmetic {
+    public static void main(String[]args){
+        int a=12, b=5;
+
+        //addition operator
+        System.out.println("a+b is:"+(a+b));
+
+        //subtraction operator
+        System.out.println("a-b is:"+(a-b));
+
+        //multiplication operator
+        System.out.println("a*b is:"+(a*b));
+
+        //division operator
+        System.out.println("a/b is:"+(a/b));
+
+        //modula operator
+        System.out.println("a%b is:"+(a%b));
+    } 
+}
