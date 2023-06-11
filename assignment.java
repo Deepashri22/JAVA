b72afc6e8118f268b242966096fb25a15035eb31
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-11 16:38:18 +0530

    assignment operator

---
+++ assignment.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class assignment {
+    public static void main(String[]args){
+        int a=4;
+        int var;
+        //assign value using =
+        var=a;
+        System.out.println("var using=:"+var);
+
+        //assig value using =+
+        var+=a;
+        System.out.println("var using+=:"+var);
+
+        //assign value using =*
+        var*=a;
+        System.out.println("var using *=:"+var);
+
+    }
+}
