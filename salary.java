7bada7bd189bad598d57c98a74d8d1a7ec754790
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2023-06-15 17:40:35 +0530

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,14 @@
+public class salary {
+    //a program to calculate gross salaryJava
+
+	public static void main(String arg[])	
+	{
+	    double grossSalary=10000;
+	    double incomeTax=1000; 
+	    double	 providentFund=800;    
+	    double professionalTax=500;
+	    double netSalary=grossSalary-(incomeTax+providentFund+professionalTax);
+	    System.out.println("Net Salary is="+netSalary);
+                   }
+
+}
